78494b0270fcfb605e442102a6cf13d7db516cff
htetaunglynn94 <htetaunglynn94@gmail.com>
2023-12-13 09:09:21 +0630

    updated files

---
+++ openpyxl/db_classes.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Sale:
+    quantity: int
+
+@dataclass
+class Product:
+    id: str
+    name: str
+    sales: List[Sale]
